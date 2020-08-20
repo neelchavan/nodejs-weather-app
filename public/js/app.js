28e71a3d951aafeaf8de8c0822d618cd073893ac
neelchavan <neelchavan9@gmail.com>
2020-08-20 19:17:41 +0530

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Hiiiiiiiiiiiiiiiiii')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#m-1')
